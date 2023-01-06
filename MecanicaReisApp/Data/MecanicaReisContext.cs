5cd367ff2a346e1ca6ae9a2d3db4383dac6a2bfb
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-01-06 09:40:35 -0300

    Created DataBase Context

---
+++ MecanicaReisApp/Data/MecanicaReisContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MecanicaReisApp.Models.Entities;
+
+namespace MecanicaReisApp.Data
+{
+    public class MecanicaReisContext : DbContext
+    {
+        public MecanicaReisContext (DbContextOptions<MecanicaReisContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MecanicaReisApp.Models.Entities.Client> Client { get; set; }
+    }
+}
